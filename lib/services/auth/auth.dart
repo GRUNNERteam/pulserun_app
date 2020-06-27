72df5ffc0b6a13c5569ad182034aa5fc292a3a06
zNext666 <alonereview@gmail.com>
2020-06-27 07:14:38 +0700

    add signInwithGoogleAccount

--- lib/services/auth/auth.dart
+++ lib/services/auth/auth.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:pulserun_app/models/user.dart';
 
 class AuthService {
-
   // Data is store in Firebase instance
   final FirebaseAuth _auth = FirebaseAuth.instance;
   // Sign In with Email and Password
@@ -12,7 +13,6 @@ class AuthService {
           email: email, password: password);
       UserModel user = UserModel(result);
       return user;
-
     } catch (e) {
       print(e.toString());
       return null;
@@ -33,12 +33,37 @@ class AuthService {
   }
 
   // Sign Out
-  Future signOutInstance() async{
-    try{
+  Future signOutInstance() async {
+    try {
       // no need to return a result from firebase
       // just sign out Instance
       await _auth.signOut();
-    }catch(e){
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signInWithGoogleAccount(BuildContext context) async {
+    // Gain Access to google login before put user into firebase auth
+    GoogleSignIn _googleSignIn = GoogleSignIn(
+      scopes: [
+        'https://www.googleapis.com/auth/contacts.readonly',
+      ],
+    );
+    GoogleSignInAccount user = await _googleSignIn.signIn();
+    GoogleSignInAuthentication userAuth = await user.authentication;
+
+    // Put user into firebase auth
+    try {
+      AuthResult result = await _auth.signInWithCredential(
+          GoogleAuthProvider.getCredential(
+              idToken: userAuth.idToken, accessToken: userAuth.accessToken));
+      // TODO: return uid to current context/screens
+      UserModel user = UserModel(result);
+      return user; // return only needed infomation
+
+    } catch (e) {
       print(e.toString());
       return null;
     }

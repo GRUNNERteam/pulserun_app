c9e5e7cc271af6999cee99a33908326e6f7d832f
zNext666 <alonereview@gmail.com>
2020-08-26 22:41:32 +0700

    update service name

--- lib/screens/BLE/BLE.dart
+++ lib/screens/BLE/BLE.dart
@@ -2,7 +2,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:pulserun_app/services/BLE_HeartRate/widgets.dart';
 
 class BLE extends StatelessWidget {
   // This widget is the root of your application.
